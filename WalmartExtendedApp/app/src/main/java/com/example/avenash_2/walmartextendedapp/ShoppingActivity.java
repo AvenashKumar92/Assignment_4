412767949bd7e89b71eae392e51095800f35ea72
AvenashKumar <avenash_kumar@hotmail.com>
2018-04-08 13:12:21 -0500

    Fix defects/issues: Assignment 4 - Problem 2 - Task5 and 6

--- WalmartExtendedApp/app/src/main/java/com/example/avenash_2/walmartextendedapp/ShoppingActivity.java
+++ WalmartExtendedApp/app/src/main/java/com/example/avenash_2/walmartextendedapp/ShoppingActivity.java
@@ -1,6 +1,12 @@
 package com.example.avenash_2.walmartextendedapp;
 
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -18,9 +24,69 @@ public class ShoppingActivity extends AppCompatActivity {
         Intent intent = getIntent();
         String userEmail = intent.getStringExtra(String.valueOf(R.id.txtEmail));
         lblWelcome.setText(lblWelcome.getText()+" "+userEmail);
+
+        ActionBar actionBar=getSupportActionBar();
+        actionBar.setBackgroundDrawable(new ColorDrawable(Color.GREEN));
+        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+
+        ActionBar.TabListener listener=new ActionBar.TabListener() {
+            @Override
+            public void onTabSelected(ActionBar.Tab tab, FragmentTransaction ft) {
+                Intent intent = null;
+                if(tab.getText().equals("Beauty"))
+                    intent = new Intent(getApplicationContext(),BeautyProductActivity.class);
+                if(tab.getText().equals("Clothing"))
+                    intent = new Intent(getApplicationContext(),ClothActivity.class);
+                if(tab.getText().equals("Food"))
+                    intent=new Intent(getApplicationContext(), FoodActivity.class);
+                if(tab.getText().equals("Electronics"))
+                    intent =new Intent(getApplicationContext(), ElectronicsActivity.class);
+                if(tab.getText().equals("Logout"))
+                    alert();
+
+                if(intent!=null)
+                    startActivity(intent);
+            }
+
+            @Override
+            public void onTabUnselected(ActionBar.Tab tab, FragmentTransaction ft) {
+
+            }
+
+            @Override
+            public void onTabReselected(ActionBar.Tab tab, FragmentTransaction ft) {
+
+            }
+        };
+
+        actionBar.addTab(actionBar.newTab().setText("Beauty").setTabListener(listener));
+        actionBar.addTab(actionBar.newTab().setText("Clothing").setTabListener(listener));
+        actionBar.addTab(actionBar.newTab().setText("Food").setTabListener(listener));
+        actionBar.addTab(actionBar.newTab().setText("Electronics").setTabListener(listener));
+        actionBar.addTab(actionBar.newTab().setText("Logout").setTabListener(listener));
     }
 
-    public void OnImageClick(View view) {
+    public void alert(){
+        AlertDialog.Builder builder=new AlertDialog.Builder(this);
+        builder.setTitle("Alert Message");
+        builder.setMessage("Do you want to Exit?");
+        builder.setIcon(R.drawable.alerticon);
+        AlertDialog.OnClickListener listener=new AlertDialog.OnClickListener(){
+
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+                if(which==dialog.BUTTON_POSITIVE) {
+                    finish();
+                }
+            }
+        };
+        builder.setPositiveButton("Yes", listener);
+        builder.setNegativeButton("No", listener);
+        builder.show();
+    }
+
+    /*public void OnImageClick(View view) {
         final int id=view.getId();
         Intent intent = null;
         switch(id){
@@ -39,5 +105,5 @@ public class ShoppingActivity extends AppCompatActivity {
         }
         if(intent!=null)
             startActivity(intent);
-    }
+    }*/
 }
